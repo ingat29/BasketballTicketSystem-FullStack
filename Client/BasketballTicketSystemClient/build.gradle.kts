87e5077ef62319ad1635830c366548619b1cf56f
ingat29 <ignatdavid290@gmail.com>
2026-04-19 12:07:31 +0300

    Project setup for client and server to work with Protobuf, and started working on the .proto file

--- Client/BasketballTicketSystemClient/build.gradle.kts
+++ Client/BasketballTicketSystemClient/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.google.protobuf:protobuf-java:3.25.1")
 }
 
 //configure JavaFX
